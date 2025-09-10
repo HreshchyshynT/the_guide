0981a0d394f44ed55e1f07edd7f0d5229d6744ef
HreshchyshynT <hreshchyshyn.t@gmail.com>
2025-09-10 19:41:06 +0300

    add base dependencies

--- bin/the_guide.dart
+++ bin/the_guide.dart
@@ -1,57 +1,59 @@
-import 'package:args/args.dart';
+import "package:args/args.dart";
+import "package:dotenv/dotenv.dart";
 
-const String version = '0.0.1';
+const String version = "0.0.1";
 
 ArgParser buildParser() {
   return ArgParser()
     ..addFlag(
-      'help',
-      abbr: 'h',
+      "help",
+      abbr: "h",
       negatable: false,
-      help: 'Print this usage information.',
+      help: "Print this usage information.",
     )
     ..addFlag(
-      'verbose',
-      abbr: 'v',
+      "verbose",
+      abbr: "v",
       negatable: false,
-      help: 'Show additional command output.',
+      help: "Show additional command output.",
     )
-    ..addFlag('version', negatable: false, help: 'Print the tool version.');
+    ..addFlag("version", negatable: false, help: "Print the tool version.");
 }
 
 void printUsage(ArgParser argParser) {
-  print('Usage: dart the_guide.dart <flags> [arguments]');
+  print("Usage: dart the_guide.dart <flags> [arguments]");
   print(argParser.usage);
 }
 
 void main(List<String> arguments) {
+  DotEnv().load();
   final ArgParser argParser = buildParser();
   try {
     final ArgResults results = argParser.parse(arguments);
     bool verbose = false;
 
     // Process the parsed arguments.
-    if (results.flag('help')) {
+    if (results.flag("help")) {
       printUsage(argParser);
       return;
     }
-    if (results.flag('version')) {
-      print('the_guide version: $version');
+    if (results.flag("version")) {
+      print("the_guide version: $version");
       return;
     }
-    if (results.flag('verbose')) {
+    if (results.flag("verbose")) {
       verbose = true;
     }
 
     // Act on the arguments provided.
-    print('Positional arguments: ${results.rest}');
+    print("Positional arguments: ${results.rest}");
     if (verbose) {
-      print('[VERBOSE] All arguments: ${results.arguments}');
+      print("[VERBOSE] All arguments: ${results.arguments}");
     }
   } on FormatException catch (e) {
     // Print usage information if an invalid argument was provided.
     print(e.message);
-    print('');
+    print("");
     printUsage(argParser);
   }
 }
