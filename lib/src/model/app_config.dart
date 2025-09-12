15274ebc1a53b9472e4ef072dc755a54259a46af
HreshchyshynT <hreshchyshyn.t@gmail.com>
2025-09-12 11:20:55 +0300

    pivot to flutter because writing TUI in dart is fucking hell

---
+++ lib/src/model/app_config.dart
@@ -0,0 +1,28 @@
+import "package:dotenv/dotenv.dart";
+
+class AppConfig {
+  AppConfig._({required this.geminiApiKey});
+
+  static AppConfig? _instance;
+
+  AppConfig get instance {
+    final obj = _instance;
+    if (obj == null) {
+      throw Exception("AppConfig is not initialized");
+    }
+    return obj;
+  }
+
+  final String geminiApiKey;
+
+  static void init() {
+    final env = DotEnv()..load();
+    final apiKey = env["GEMINI_KEY"];
+
+    assert(apiKey != null, "gemini api key must be provided");
+
+    _instance = AppConfig._(
+      geminiApiKey: apiKey!,
+    );
+  }
+}
---
+++ lib/src/msg_constants.dart
@@ -0,0 +1,13 @@
+const welcomeMessage = """Welcome to The Guide.
+
+This is an AI-powered text adventure where the story unfolds in real-time,
+generated just for you. Every description, character, and outcome is created
+dynamically, making your adventure truly unique.
+
+Getting started is easy:
+
+1.  Choose a Story: Pick a starting scenario from the list.
+2.  Name Your Character: Tell us who you are.
+3.  Enjoy the Journey: Read the story and make choices to shape your destiny.
+
+""";
