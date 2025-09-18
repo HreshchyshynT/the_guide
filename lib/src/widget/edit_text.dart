50c5d0ff7122fdd64390869bdd5056e1b20ef148
HreshchyshynT <hreshchyshyn.t@gmail.com>
2025-09-18 23:10:56 +0300

    add edit text widget as basic input field

---
+++ lib/src/widget/edit_text.dart
@@ -0,0 +1,74 @@
+import "package:flutter/material.dart";
+import "package:flutter/services.dart";
+
+class EditText extends StatelessWidget {
+  const EditText({
+    super.key,
+    this.maxInputLength,
+    this.autofocus = false,
+    this.hintText,
+    this.controller,
+    this.textStyle,
+    this.hintStyle,
+    this.textCapitalization = TextCapitalization.words,
+    this.textAlign = TextAlign.center,
+    this.keyboardType,
+    this.obscureText = false,
+    this.suffixIcon,
+    this.suffixIconColor,
+    this.textAlignVertical = TextAlignVertical.center,
+    this.autofillHints,
+    this.textInputAction,
+    this.onEditingComplete,
+    this.contentPadding = EdgeInsets.zero,
+  });
+
+  final int? maxInputLength;
+  final bool autofocus;
+  final String? hintText;
+  final TextEditingController? controller;
+  final TextStyle? textStyle;
+  final TextStyle? hintStyle;
+  final TextCapitalization textCapitalization;
+  final TextAlign textAlign;
+  final TextInputType? keyboardType;
+  final bool obscureText;
+  final Widget? suffixIcon;
+  final Color? suffixIconColor;
+  final TextAlignVertical textAlignVertical;
+  final Iterable<String>? autofillHints;
+  final TextInputAction? textInputAction;
+  final VoidCallback? onEditingComplete;
+  final EdgeInsets contentPadding;
+
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+      inputFormatters: [
+        if (maxInputLength != null)
+          LengthLimitingTextInputFormatter(maxInputLength),
+      ],
+      obscureText: obscureText,
+      textAlign: textAlign,
+      controller: controller,
+      textCapitalization: textCapitalization,
+      keyboardType: keyboardType,
+      cursorRadius: const Radius.circular(4),
+      style: textStyle,
+      autofocus: autofocus,
+      textAlignVertical: TextAlignVertical.center,
+      decoration: InputDecoration(
+        hintText: hintText,
+        isDense: true,
+        contentPadding: contentPadding,
+        hintStyle: hintStyle,
+        border: InputBorder.none,
+        suffixIcon: suffixIcon,
+        suffixIconColor: suffixIconColor,
+      ),
+      autofillHints: autofillHints,
+      textInputAction: textInputAction,
+      onEditingComplete: onEditingComplete,
+    );
+  }
+}
