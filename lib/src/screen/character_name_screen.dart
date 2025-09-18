9efdb78aa15db72a296d23b0ca116b4609789d03
HreshchyshynT <hreshchyshyn.t@gmail.com>
2025-09-18 23:11:32 +0300

    add very basic entring name and confirmation dialog

--- lib/src/screen/character_name_screen.dart
+++ lib/src/screen/character_name_screen.dart
@@ -3,6 +3,7 @@ import "package:the_guide/src/ai/ai_client.dart";
 import "package:the_guide/src/common/prompts.dart";
 import "package:the_guide/src/model/app_config.dart";
 import "package:the_guide/src/model/story_intro.dart";
+import "package:the_guide/src/widget/edit_text.dart";
 
 class CharacterNameScreen extends StatefulWidget {
   const CharacterNameScreen({
@@ -30,11 +31,18 @@ class CharacterNameScreen extends StatefulWidget {
 
 class _CharacterNameScreenState extends State<CharacterNameScreen> {
   String? story;
+  late final TextEditingController editTextController = TextEditingController();
 
   @override
   void initState() {
     super.initState();
     _generateIntroduction();
+  }
+
+  @override
+  void dispose() {
+    editTextController.dispose();
+    super.dispose();
   }
 
   @override
@@ -49,13 +57,63 @@ class _CharacterNameScreenState extends State<CharacterNameScreen> {
           vertical: 24.0,
           horizontal: 40.0,
         ),
-        child: Center(
-          child:
-              story != null ? Text(story) : const CircularProgressIndicator(),
+        child: Column(
+          children: [
+            Expanded(
+              child: story != null
+                  ? Text(story)
+                  : const Center(
+                      child: CircularProgressIndicator(),
+                    ),
+            ),
+            EditText(
+              hintText: "Enter your name",
+              textInputAction: TextInputAction.done,
+              controller: editTextController,
+              onEditingComplete: () async {
+                FocusScope.of(context).unfocus();
+                final name = editTextController.text;
+                // TODO: add min length limitation
+                if (name.isEmpty) return;
+                final confirmed = await _displayConfirmStartDialog(name);
+                if (confirmed == true) {
+                  // TODO: move on
+                }
+              },
+            ),
+          ],
         ),
       ),
     );
   }
+
+  Future<bool?> _displayConfirmStartDialog(String name) => showDialog<bool>(
+        context: context,
+        barrierDismissible: true,
+        builder: (context) => AlertDialog(
+          title: Text("$name, let's start?"),
+          actions: <Widget>[
+            TextButton(
+              style: TextButton.styleFrom(
+                textStyle: Theme.of(context).textTheme.labelLarge,
+              ),
+              child: const Text("Not yet"),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+            TextButton(
+              style: TextButton.styleFrom(
+                textStyle: Theme.of(context).textTheme.labelLarge,
+              ),
+              child: const Text("OK"),
+              onPressed: () {
+                Navigator.of(context).pop(true);
+              },
+            ),
+          ],
+        ),
+      );
 
   void _generateIntroduction() {
     final client = AiClient(apiKey: AppConfig.instance.geminiApiKey);
